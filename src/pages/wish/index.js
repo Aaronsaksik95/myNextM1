21364df6d4e2905a2e0ee44d70a404acbdfd457a
Aaron Saksik <aaron.saksik@ynov.com>
2022-02-10 00:00:16 +0100

    Module not found Wish

--- src/pages/wish/index.js
+++ src/pages/wish/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react';
 import styles from "./index.module.scss";
-import Grid from '../../components/grid/Grid';
+// import Grid from '../../components/grid/Grid';
 
 
 const index = () => {
