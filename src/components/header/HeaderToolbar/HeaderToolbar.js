487bf2cc2d4664f587f374ec13afec7f8e52d528
Aaron Saksik <aaron.saksik@ynov.com>
2022-02-09 23:48:45 +0100

    Fix search useQuery

--- src/components/header/HeaderToolbar/HeaderToolbar.js
+++ src/components/header/HeaderToolbar/HeaderToolbar.js
@@ -1,24 +1,10 @@
 import React, { useContext } from 'react';
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "./HeaderToolbar.module.scss";
-import { useQuery } from "@apollo/react-hooks";
 import { useRouter } from 'next/router';
 
 const Headertoolbar = () => {
     const router = useRouter()
-
-    const searchMovie = (value) => {
-        const { loading, error, data } = useQuery(getCategories);
-        if (loading) {
-            return "loading...";
-        }
-
-        if (error) {
-            console.log(error);
-            return null;
-        }
-    }
-
     return (
         <div className={styles.header__toolbar}>
             <div>
