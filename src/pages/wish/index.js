6dc64652fbbd4fd818fa1419ed0d21f6c2ae410c
Aaron Saksik <aaron.saksik@ynov.com>
2022-02-16 01:09:30 +0100

    Implement WishList

--- src/pages/wish/index.js
+++ src/pages/wish/index.js
@@ -1,13 +1,25 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styles from "./index.module.scss";
-// import Grid from '../../components/grid/Grid';
+import SimpleGrid from '../../components/grid/simpleGrid/SimpleGrid';
+import wishService from '../../services/wish.service';
 
 
 const index = () => {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        wishService
+            .getWish(token)
+            .then((data) => {
+                console.log(data.wish.movies)
+                setMovies(data.wish.movies)
+            })
+    }, [])
+
     return (
         <div className={styles.shop}>
             <div className={styles.grid__shop}>
-                {/* <Grid movies={} /> */}
+                <SimpleGrid movies={movies} />
             </div>
         </div>
     );
--- src/services/wish.service.js
+++ src/services/wish.service.js
@@ -1,50 +1,38 @@
+import nextConfig from "../../next.config"
+
 export default {
-    add_wish(movie) {
-        const newItem = {
-            id: movie.id,
-            name: movie.name,
-            price: movie.price,
-            image: movie.image
-        }
-        var wish = JSON.parse(localStorage.getItem('wish')) || [];
-        const alreadyExisting = (el) => el.id === movie.id;
-        const index = wish.findIndex(alreadyExisting);
-        if (index !== -1) {
-            localStorage.setItem('wish', JSON.stringify(wish))
-        }
-        else {
-            wish.push(newItem)
-            localStorage.setItem('wish', JSON.stringify(wish))
-        }
+    async addWish(movie, token) {
+        return await fetch(`${nextConfig.env.API_URL}wish/`, {
+            method:"POST",
+            body:{
+                movie: movie
+            },
+            headers: {
+                "content-type": "application/json",
+                "authorization": token
+            },
+        }).then((res) => res.json())
+    },
+    
+    async getWish(token) {
+        return await fetch(`${nextConfig.env.API_URL}wish/`, {
+            headers: {
+                "content-type": "application/json",
+                "authorization": token
+            },
+        }).then((res) => res.json())
     },
 
-    get_wish() {
-        return JSON.parse(localStorage.getItem('wish'))
-    },
-    verify_movie_in_wish(id) {
-        var wish = JSON.parse(localStorage.getItem('wish'))
-        const alreadyExisting = (el) => el.id === movie.id;
-        const index = wish.findIndex(alreadyExisting);
-        if (index == -1){
-            return true
-        }
-    },
-    remove_item_wish(movie) {
-        var wish = JSON.parse(localStorage.getItem('wish'))
-        const removeWithIndex = (el) => el.id === movie.id;
-        const index = wish.findIndex(removeWithIndex);
-        wish.splice(index, 1)
-        localStorage.setItem('wish', JSON.stringify(wish))
-        if (wish.length == 0) {
-            this.remove_wish()
-        }
-        else {
-            document.location.reload();
-        }
-    },
-
-    remove_wish() {
-        localStorage.removeItem('wish');
-        document.location.reload();
+    async deleteOneMovie(movie, token) {
+        return await fetch(`${nextConfig.env.API_URL}wish/`, {
+            method: "PUT",
+            body:{
+                movie: movie
+            },
+            headers: {
+                "content-type": "application/json",
+                "authorization": token
+            },
+        }).then((res) => res.json())
     },
 }
